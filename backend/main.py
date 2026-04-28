e48cfc0fb3af6a12f060bd51724d7ac60c5dfd65
Patrick Thompson <229507349+PatThompson1306@users.noreply.github.com>
2026-04-28 16:12:49 +0100

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+#imports
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+# create the app and mount the static files
+app = FastAPI()
+app.mount("/frontend", StaticFiles(directory = "../frontend"), name = "frontend")
+
+# define the root endpoint to serve the index.html file
+@app.get("/")
+def root():
+    return FileResponse("../frontend/index.html")
